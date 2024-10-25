93a983a5e6cc1eaf1114df514413c243883772ca
alfie1921 <178087972+Alfie1921@users.noreply.github.com>
2024-10-25 21:05:27 +0100

    completed exercise, tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const fourYears = year % 4 === 0;
+    const oneHundresYears = year % 100 === 0;
+    const fourHundredYears = year % 400 === 0;
+
+    if (fourYears && (!oneHundresYears || fourHundredYears)) {
+        console.log('true')
+        return(true);
+    }   else {
+        console.log('false')
+        return(false);
+    }
 
 };
-
+leapYears(240)
 // Do not edit below this line
 module.exports = leapYears;
