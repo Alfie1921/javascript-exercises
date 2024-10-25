cdee5a73281f6eefc4a8cfe352f2e3e6a978e3bb
alfie1921 <178087972+Alfie1921@users.noreply.github.com>
2024-10-25 17:13:12 +0100

    completed exercise, tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetition) {
+    let words = ''
+    if (repetition < 0) {
+        console.log('ERROR')
+        return ('ERROR')
+    }
+    for (i=0; i < repetition; i++) {
+        words += string
+    }
+    console.log(words)
+    console.log(number)
+    return words
 };
-
+const number = Math.floor(Math.random() * 1000)
+repeatString('hey', 3)
+repeatString('hello', 10)
+repeatString('hi', 1)
+repeatString('bye, 0')
+repeatString('goodbye', -1)
+repeatString('odin', number)
+repeatString('', 10)
 // Do not edit below this line
 module.exports = repeatString;
