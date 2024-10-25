8928924d424f8437c27c5f6d90f13a022de119af
alfie1921 <178087972+Alfie1921@users.noreply.github.com>
2024-10-25 20:17:49 +0100

    completed exercise, passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+
+const arr = [1, 2, 3, 4];
+const removeFromArray = function(array, ...values) {
+    return array.filter(item => !values.includes(item));
 };
+
+const result = removeFromArray(arr, 3, 2)
+console.log(result)
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
