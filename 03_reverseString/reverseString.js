47b3417bbd2765393cdfd8170c71bd10babf17c0
alfie1921 <178087972+Alfie1921@users.noreply.github.com>
2024-10-25 17:33:24 +0100

    exercise completed, tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversed = ''
+    for(i = string.length - 1; i >= 0; i--) {
+        reversed += string[i]
+    
+    }
+    console.log(reversed)
+    return(reversed)
+}
 
-};
-
+reverseString('hello')
+reverseString('hello there')
+reverseString('123! abc! Hello, Odinite')
+reverseString('')
 // Do not edit below this line
 module.exports = reverseString;
