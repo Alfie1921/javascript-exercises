35e4bdfc2117edaf844d4b859568666bfd787094
alfie1921 <178087972+Alfie1921@users.noreply.github.com>
2024-10-25 20:50:56 +0100

    exercise completed, tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
 
+const sumAll = function(lower, higher) {
+    let sum = 0;
+    if (higher < 0 || lower < 0) {
+        return('ERROR');
+    }
+    if (lower > higher) {
+        swap = lower;
+        lower = higher;
+        higher = swap;
+    }
+    if (!Number.isInteger(lower) || !Number.isInteger(higher)) {
+        return('ERROR')
+    }
+    for(i = lower; i <= higher; i++) {
+        sum += i; 
+    }
+    console.log(sum);
+    return sum;
 };
-
+sumAll(1, 4)
+sumAll(1, 100)
 // Do not edit below this line
 module.exports = sumAll;
